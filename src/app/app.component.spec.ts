e0d5cc457ec3bc90cc9bcf77cb060b8b75e7d958
Kacper <kacpersjuszb@gmail.com>
2023-09-13 08:48:12 +0200

    idk

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,6 +7,7 @@ describe('AppComponent', () => {
 
   }));
 
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
