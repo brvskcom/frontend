be1eb5938259cc06e04e8386ddbdba8df1ce1c47
Kacper <kacpersjuszb@gmail.com>
2023-09-14 12:16:43 +0200

    search for Products by Category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
-import { map } from 'rxjs/operators';
+import {catchError, map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category'
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,22 @@ import { map } from 'rxjs/operators';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/v1/products';
+  private categoryUrl = 'http://localhost:8080/api/v1/categories';
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
-    // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.httpClient.get(searchUrl).pipe(
-      map((response: any) => response.content) // Wydobywanie zawartości z paginowanej odpowiedzi
+      map((response: any) => response.content)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get(this.categoryUrl).pipe(
+      map((response: any) => response)
     );
   }
 }
